d3c903c39877ee2245c9b8ef359726d25caec11e
wengh <whyharry9@gmail.com>
2021-10-29 16:44:31 -0400

    put stuff in a class

--- main.py
+++ main.py
@@ -9,101 +9,91 @@ from bleak.backends.scanner import AdvertisementData
 from bleak.exc import BleakError
 
 
-MANUFACTURER_DATA = {0xffff: b'$tZuFTNvsLGt9U^gsCM!t8$@Fd6'}
+class NsBleClient:
 
-SERVICE_UUID      = '00000000-b1b6-417b-af10-da8b3de984be'
-BRIGHTNESS_UUID   = '00000001-b1b6-417b-af10-da8b3de984be'
-VOLUME_UUID       = '00000002-b1b6-417b-af10-da8b3de984be'
-PAUSE_VOLUME_UUID = '10000001-b1b6-417b-af10-da8b3de984be'
+    MANUFACTURER_DATA = {0xffff: b'$tZuFTNvsLGt9U^gsCM!t8$@Fd6'}
 
-LUX_TO_NITS = 1 / math.pi
+    SERVICE_UUID = '00000000-b1b6-417b-af10-da8b3de984be'
+    BRIGHTNESS_UUID = '00000001-b1b6-417b-af10-da8b3de984be'
+    VOLUME_UUID = '00000002-b1b6-417b-af10-da8b3de984be'
+    PAUSE_VOLUME_UUID = '10000001-b1b6-417b-af10-da8b3de984be'
 
+    LUX_TO_NITS = 1 / math.pi
 
-def encode(string: str) -> bytes:
-    return string.encode('utf-8')
+    @staticmethod
+    def encode(string: str) -> bytes:
+        return string.encode('utf-8')
 
+    @staticmethod
+    def decode(byte_array: bytearray) -> str:
+        return byte_array.decode('utf-8')
 
-def decode(byte_array: bytearray) -> str:
-    return byte_array.decode('utf-8')
+    async def find_device(self) -> BLEDevice:
+        print('Discovering devices...')
+        async with BleakScanner() as scanner:
 
+            start_time = time.time()
+            future = asyncio.Future()
+            detected = set()
 
-async def find_device() -> BLEDevice:
-    print('Discovering devices...')
-    async with BleakScanner() as scanner:
+            def detection_callback(device: BLEDevice, data: AdvertisementData):
+                if device.address not in detected:
+                    detected.add(device.address)
+                    print(' ', device)
+                    if data.manufacturer_data == self.MANUFACTURER_DATA:
+                        time_used = time.time() - start_time
+                        print(f'Device found after {time_used:.2f} s')
+                        future.set_result(device)
 
-        start_time = time.time()
-        future = asyncio.Future()
-        detected = set()
+            scanner.register_detection_callback(detection_callback)
+            return await future
 
-        def detection_callback(device: BLEDevice, data: AdvertisementData):
-            if device.address not in detected:
-                detected.add(device.address)
-                print(' ', device)
-                if data.manufacturer_data == MANUFACTURER_DATA:
-                    time_used = time.time() - start_time
-                    print(f'Device found after {time_used:.2f} s')
-                    future.set_result(device)
+    def __init__(self):
+        pass
+        # self.device = None
+        # self.client = None
+        # self.service = None
+        # self.brightness_characteristic = None
+        # self.volume_characteristic = None
+        # self.pause_characteristic = None
 
-        scanner.register_detection_callback(detection_callback)
-        return await future
+    async def discover_and_connect(self):
+        self.device = await self.find_device()
 
+        print(f'Connecting to device {self.device}')
 
-async def find_device_debug() -> BLEDevice:
-    discovered = set()
-    print('Discovering devices...')
-    number_of_scans = 0
-    while True:
-        number_of_scans += 1
-        devices = await BleakScanner.discover(timeout=0.5)
-        for device in devices:
-            if device.address not in discovered:
-                discovered.add(device.address)
-                print(' ', device)
-                try:
-                    if device.metadata['manufacturer_data'][65535] == MANUFACTURER_UNIQUE_IDENTIFIER:
-                        print(f'Device found after {number_of_scans} scans')
-                        return device
-                except KeyError:
-                    pass
-                except TypeError:
-                    pass
+        self.client = BleakClient(self.device, timeout=100)
+        await self.client.connect()
+        # note: due to a bug in Windows connection will fail if we pair with the device
+        #       so never try to pair with it
+        services = await self.client.get_services()
+        self.service = services.get_service(self.SERVICE_UUID)
+
+        for c in self.service.characteristics:
+            print()
+            print(c.uuid, c.description)
+            byte_array = await self.client.read_gatt_char(c)
+            print('Read:', self.decode(byte_array))
+
+            if c.uuid == self.BRIGHTNESS_UUID:
+                self.brightness_characteristic = c
+            if c.uuid == self.VOLUME_UUID:
+                self.volume_characteristic = c
+            if c.uuid == self.PAUSE_VOLUME_UUID:
+                self.pause_characteristic = c
+
+    async def get_brightness(self):
+        raw = await self.client.read_gatt_char(self.brightness_characteristic)
+        value = self.decode(raw)
+        return value
 
 
 async def test():
-    device = await find_device()
-
-    print(f'Connecting to device {device}')
-
-    async with BleakClient(device, timeout=100) as client:
-        # note: due to a bug in Windows connection will fail if we pair with the device
-        #       so never try to pair with it
-        services = await client.get_services()
-        service = services.get_service(SERVICE_UUID)
-
-        for c in service.characteristics:
-            print()
-            print(c.uuid, c.description)
-            byte_array = await client.read_gatt_char(c)
-            print('Read:', decode(byte_array))
-
-            if c.uuid == PAUSE_VOLUME_UUID:
-                print('Testing write')
-                print('The previous read should give 0 and the next read should give 1')
-                await client.write_gatt_char(c, encode('1'))
-                print(decode(await client.read_gatt_char(c)))
-                print('Finished testing write')
-
-            elif c.uuid == BRIGHTNESS_UUID:
-
-                def callback(sender: int, data: bytearray):
-                    print(' ', f'Notified by {sender} with value {decode(data)}')
-
-                print('Testing callback...')
-                await client.start_notify(c, callback)
-                await asyncio.sleep(3)
-                await client.stop_notify(c)
-                print('Finished testing callback')
-
+    client = NsBleClient()
+    await client.discover_and_connect()
+    for i in range(10):
+        print(await client.get_brightness())
+        await asyncio.sleep(1)
 
 if __name__ == '__main__':
     asyncio.run(test())
